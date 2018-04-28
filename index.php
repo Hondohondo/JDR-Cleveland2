3e80a27386be7b7b7588ec68463f2ec311c72a4e
Hondohondo <mnandi19@jcu.edu>
2018-04-27 22:00:00 -0400

    Adding all the JDR2 Website

---
+++ index.php
@@ -0,0 +1,398 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="JDR Home Improvements is a Cleveland, Ohio Home Construction and Remodeling Company that is committed to Superior Craftsmanship and Professionalism in the Home Construction Trades.">
+        
+        <link rel="stylesheet" type="text/css" href="vendors/css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="vendors/css/grid.css">
+        <link rel="stylesheet" type="text/css" href="vendors/css/ionicons.min.css">
+        <link rel="stylesheet" type="text/css" href="vendors/css/animate.css">
+        <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+        <link rel="stylesheet" type="text/css" href="resources/css/queries.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,300i,400" rel="stylesheet">
+        <title>JDR Home Improvements</title>
+
+        
+        <link rel="apple-touch-icon" sizes="180x180" href="/resources/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/resources/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/resources/favicons/favicon-16x16.png">
+        <link rel="manifest" href="/resources/favicons/manifest.json">
+        <link rel="mask-icon" href="/resources/favicons/safari-pinned-tab.svg" color="#5bbad5">
+        <link rel="shortcut icon" href="/resources/favicons/favicon.ico">
+        <meta name="msapplication-config" content="/resources/favicons/browserconfig.xml">
+        <meta name="theme-color" content="#ffffff">
+      
+    
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="row">
+                    <!--
+                    <img src="resources/img/logo.png" alt="JDR logo" class="logo">
+                   
+                    <i class="ion-ios-home logo"></i>
+                    -->
+                    
+                     <i class="ion-ios-home logo"><h5>JDR</h5></i>
+                     <i class="ion-ios-home-outline logo-black"><h6>JDR</h6></i>
+                    
+                    
+                    
+                    
+                    <ul class="main-nav js--main-nav">
+                        <li><a href="#features">Home</a></li>
+                        <li><a href="#about">About us</a></li>
+                        <li><a href="#services">Services</a></li>
+                        <li><a href="#contact">Contact us</a></li>
+                    </ul>
+                    
+                    <a class="mobile-nav-icon js--nav-icon"><i class="ion-navicon-round"></i></a>
+                
+                </div>
+                
+            </nav>
+            <div class="jdr-text-box">
+                <h1>Cleveland Construction, <br> Home Improvements and Remodeling</h1>
+                <a class="btn btn-full js--scroll-to-services" href="#">Services</a>
+                <a class="btn btn-ghost js--scroll-to-start" href="#">Show me more</a>
+            </div>
+        </header>
+               
+        
+        <section class="section-features js--section-features" id="features">
+            <div class="row">
+                <h2>JDR Home Improvements &mdash; Cleveland, OH</h2>
+                
+                <p class="long-copy">
+                    
+                   JDR Home Improvements is a Cleveland, Ohio Home Construction and Remodeling Company that is committed to Superior Craftsmanship and Professionalism in the Home Construction Trades. Working in the greater Cleveland area, we specialize in Additions and the Remodeling of Kitchens and Baths, Basements, Decks, as well as Carpentry and General Contracting Services.
+                
+                </p>
+            </div>
+            
+            <div class="row js--wp-1">
+                <div class="col span-1-of-4 box">
+                    <i class="ion-ios-lightbulb-outline icon-big"></i>
+                    
+                    <h3>Long-time Experience</h3> 
+                    <p>
+                    We are a general contracting company and although relatively new, our workforce has over 30 years of experience in all aspects of home remodeling, new construction and maintenance. Our job tomorrow depends on how we do our work today.
+                    </p>
+                </div>
+                
+                <div class="col span-1-of-4 box">
+                    <i class="ion-ribbon-b icon-big"></i>
+                    <h3>Focus on Quality</h3> 
+                    <p>
+                    Every job, no matter how big, is approached with same respect and professionalism. Since early 2007 our work has been based solely on referrals. We guarantee all our work and customer communication is of great importance to us. We are patient, and we aim to perform above expectations at all times.
+                    </p>
+                </div>
+                
+                <div class="col span-1-of-4 box">
+                    <i class="ion-ios-pricetags-outline icon-big"></i>
+                    <h3>Fees and Pricing</h3> 
+                    <p>
+                    Preliminary estimates are free and detailed estimates and all pricing is explained in the process of interviewing and drawing up the proposal. We use only licensed Plumbers, Electricians and HVAC trades. All other work we perform ourselves.
+                    </p>
+                </div>
+                
+                <div class="col span-1-of-4 box">
+                    <i class="ion-ios-heart icon-big"></i>
+                    <h3>Passion for what we do</h3> 
+                    <p>
+                    All of us at JDR Home Improvements love what we do, and take great pride in the finished product. We are open to working with any budget and will find creative ways to making our customers' visions come to life.
+                    </p>
+                </div>
+            </div>
+        </section>
+        
+        <section class="section-photos">
+        
+            <ul class="photos-showcase clearfix">
+                <li>
+                    <figure class="photo">
+                        <img src="resources/img/1.jpg" alt="deck">
+                    </figure>
+                </li>
+                
+                 <li>
+                    <figure class="photo">
+                        <img src="resources/img/2.jpg" alt="deck">
+                    </figure>
+                </li>
+                
+                 <li>
+                    <figure class="photo">
+                        <img src="resources/img/3.jpg" alt="deck">
+                    </figure>
+                </li>
+                
+                 <li>
+                    <figure class="photo">
+                        <img src="resources/img/11.jpg" alt="deck">
+                    </figure>
+                </li>
+            </ul>
+            
+            <ul class="photos-showcase clearfix">
+                <li>
+                    <figure class="photo">
+                        <img src="resources/img/5.jpg" alt="deck">
+                    </figure>
+                </li>
+                
+                 <li>
+                    <figure class="photo">
+                        <img src="resources/img/6.jpg" alt="sitting-room">
+                    </figure>
+                </li>
+                
+                 <li>
+                    <figure class="photo">
+                        <img src="resources/img/4.jpg" alt="bathroom">
+                    </figure>
+                </li>
+                
+                 <li>
+                    <figure class="photo">
+                        <img src="resources/img/13.jpg" alt="kitchen">
+                    </figure>
+                </li>
+            </ul>
+        </section>
+        
+        <section class="section-company" id="about">
+            <div class="row">
+                <h2>JDR Home Improvement &mdash; Short History</h2>
+            </div>
+            
+            <div class="row">
+                <div class="col span-1-of-2 company-box">
+                    <img src="resources/img/goran.jpg" alt="goran" class="goran-photo js--wp-2">
+                </div>
+                
+                <div class="col span-1-of-2 company-box">
+                    <div class="company-history">
+                        <div>1</div>
+                        <p>JDR Home Improvements is a small home-remodeling construction company established in 2007 by Goran Zekic.</p>
+                    </div>
+                    
+                    <div class="company-history">
+                        <div>2</div>
+                        <p>Goran is a Civil Engineer by trade and studied Architecture in Yugoslavia before moving to the United States.</p>
+                    </div>
+                    
+                    <div class="company-history">
+                        <div>3</div>
+                        <p>Before establishing JDR Home Improvements, Goran worked as crew foreman for a large residential remodeling company, and prior to that he worked in both commercial and residential construction.</p>
+                    </div>
+                    
+                    <a href="" class="btn-safe"><img src="resources/img/lead-safe.jpg" alt="lead-safe button"></a>
+                    
+                    <a href="" class="btn-safe"><img src="resources/img/lead-safe.jpg" alt="lead-safe button"></a>
+                </div>
+            </div>
+        </section>
+        
+        <section class="section-offer js--section-offer" id="services">
+            <div class="row">
+                <h2>Remodeling and Building Services we offer</h2>
+            </div>
+            
+            <div class="row js--wp-3">
+                <div class="col span-1-of-4 box">
+                    <img src="resources/img/14.jpg" alt="kitchen">
+                    <h3>Kitchens</h3>
+                    
+                    <div class="offer-feature">
+                        <i class="ion-ios-home-outline icon-small"></i>
+                        Kitchens
+                    </div>
+                </div>
+                
+                <div class="col span-1-of-4 box">
+                    <img src="resources/img/24.jpg" alt="bathroom">
+                    <h3>Bathrooms</h3>
+                    
+                    <div class="offer-feature">
+                        <i class="ion-ios-home-outline icon-small"></i>
+                        Bathrooms
+                    </div>
+                </div>
+                
+                <div class="col span-1-of-4 box">
+                    <img src="resources/img/23.jpg" alt="basement">
+                    <h3>Basements</h3>
+                    
+                    <div class="offer-feature">
+                        <i class="ion-ios-home icon-small"></i>
+                        Basements
+                    </div>
+                </div>
+                
+                <div class="col span-1-of-4 box">
+                    <img src="resources/img/26.jpg" alt="decks">
+                    <h3>Decks</h3>
+                    
+                    <div class="offer-feature">
+                        <i class="ion-ios-home icon-small"></i>
+                        Decks
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section class="section-testimonials">
+            <div class="row">
+                <h2>What our clients have to say</h2>
+            </div>
+            
+            <div class="row">
+                <div class="col span-1-of-3">
+                    <blockquote>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+                        <cite><img src="resources/img/Arthur%20Evans.jpg">Arthur Evans</cite>
+                    </blockquote>
+                </div>
+                
+                 <div class="col span-1-of-3">
+                    <blockquote>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+                        <cite><img src="resources/img/Glenda%20Sanchez.jpg">Glenda Sanchez</cite>
+                    </blockquote>
+                </div>
+                
+                 <div class="col span-1-of-3">
+                    <blockquote>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+                        <cite><img src="resources/img/Joel%20Riley.jpg">Joel Riley</cite>
+                    </blockquote>
+                </div>
+            </div>
+        </section>
+        
+        <section class="section-form" id="contact">
+            <div class="row" id="form">
+                <h2>We're happy to hear from you</h2>
+            </div>
+            
+            <div class="row">
+                <form method="post" action="mailer.php" class="contact-form">
+                    
+                    <div class="row">
+                        
+                        <?php
+                        
+                        if ($_GET['success'] == 1) {
+                            echo "<div class=\"form-messages success\">Thank you! Your message has been sent.</div>";
+                        }
+                        
+                        if ($_GET['success'] == -1) {
+                            echo "<div class=\"form-messages error\">Ooops! Something went wrong. Please try again!</div>";
+                        }
+                        
+                        ?>
+                    </div>
+                    
+                    <div class="row">
+                        
+                        <div class="col span-1-of-3">
+                            <label for="name">Name</label>
+                        </div>
+                        <div class="col span-2-of-3">
+                            <input type="text" name="name" id="name" placeholder="Your name" required>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col span-1-of-3">
+                            <label for="email">Email</label>
+                        </div>
+                        <div class="col span-2-of-3">
+                            <input type="email" name="email" id="email" placeholder="Your email" required>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col span-1-of-3">
+                            <label for="find-us">How did you find us?</label>
+                        </div>
+                        <div class="col span-2-of-3">
+                            <select name="find-us" id="find-us">
+                                <option value="friends" selected>Friends</option>
+                                <option value="search">Search Engine</option>
+                                <option value="ad">Advertisement</option>
+                                <option value="other">Other</option>
+                            </select>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col span-1-of-3">
+                            <label>Drop us a line</label>
+                        </div>
+                        <div class="col span-2-of-3">
+                            <textarea name="message" placeholder="Your message"></textarea>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col span-1-of-3">
+                            <label> &nbsp; </label>
+                        </div>
+                        <div class="col span-2-of-3">
+                            <input type="submit" value="Send it!" >
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
+        
+        <footer>
+            <div class="row">
+                <div class="col span-1-of-2">
+                    <ul class="footer-nav">
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Press</a></li>
+                    </ul>
+                </div>
+                
+                <div class="col span-1-of-2">
+                    <ul class="social-links">
+                        <li><a href="#"><i class="ion-social-facebook"></i></a></li>
+                        <li><a href="#"><i class="ion-social-twitter"></i></a></li>
+                        <li><a href="#"><i class="ion-social-googleplus"></i></a></li>
+                        <li><a href="#"><i class="ion-social-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div class="row">
+                <p>
+                    Copyright &copy; 2017 by JDR Home Improvements. All rights reserved. 
+                </p>
+            </div>
+        </footer>
+        
+        
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/respond@0.9.0/main.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/html5shiv@3.7.3/dist/html5shiv.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/selectivizr@1.0.3/selectivizr.min.js"></script>
+    <script src="vendors/js/jquery.waypoints.min.js"></script>
+    <script src="resources/js/script.js"></script>
+    </body>
+    
+    
+    
+    
+    
+    
+
+</html>
